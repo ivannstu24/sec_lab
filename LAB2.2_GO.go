c17f5e1654e3440ea7124964c705f4012225235c
ivannstu24 <161632314+ivannstu24@users.noreply.github.com>
2024-04-05 22:56:10 +0700

    Update LAB2.2_GO.go

--- LAB2.2_GO.go
+++ LAB2.2_GO.go
@@ -1,20 +1,26 @@
-//задание 1
+/* 
+// Задание 1
 
-/*
 package main
 
 import "fmt"
 
+// Функция для подсчета количества непосещенных камней
 func unvisitedStones(N, stones int, birds []int) int {
+    // Создаем срез для хранения информации о посещенных камнях, инициализируем его нулями
 	visited := make([]int, stones)
+    // Проходим по каждой птице в списке
 	for _, bird := range birds {
+        // Проходим по каждому камню от 1 до количества камней
 		for i := 1; i <= stones; i++ {
+            // Если номер камня делится на номер птицы без остатка, увеличиваем счетчик посещений для этого камня
 			if i%bird == 0 {
 				visited[i-1]++
 			}
 		}
 	}
 
+    // Считаем количество непосещенных камней
 	unvisitedCount := 0
 	for _, count := range visited {
 		if count == 0 {
@@ -26,6 +32,7 @@ func unvisitedStones(N, stones int, birds []int) int {
 }
 
 func main() {
+    // Пример вызова функции и вывод результатов
 	fmt.Println(unvisitedStones(2, 6, []int{3, 2})) // Результат: 2
 	fmt.Println(unvisitedStones(2, 6, []int{5, 1})) // Результат: 0
 }
@@ -34,7 +41,9 @@ func main() {
 
 
 /*
-//Задание 2
+
+// Задание 2
+
 package main
 
 import (
@@ -42,10 +51,13 @@ import (
 	"strings"
 )
 
+// Функция для подсчета количества уникальных email адресов
 func numUniqueEmails(emails []string) int {
 	uniqueEmails := make(map[string]bool)
 
+    // Проходим по каждому email в массиве
 	for _, email := range emails {
+        // Разделяем email на локальную часть и домен
 		parts := strings.Split(email, "@")
 		local := parts[0]
 		domain := parts[1]
@@ -66,6 +78,7 @@ func numUniqueEmails(emails []string) int {
 }
 
 func main() {
+    // Пример входных данных
 	emails := []string{
 		"merzovik69@mail.ru",
 		"merzo.vik.69@mail.ru",
@@ -74,14 +87,16 @@ func main() {
 		"helpme69@mail.ru",
 		"help.me69@mail.ru",
 	}
+    // Выводим результат работы функции
 	fmt.Println(numUniqueEmails(emails)) // Вывод: 2
 }
+
 */
 
 
 
 /*
-//Задание 3
+// Задание 3
 
 package main
 
@@ -90,12 +105,17 @@ import (
 	"strconv"
 )
 
+// Функция для подсчета суммы цифр чисел, делящихся на 3
 func sumOfDivisibleByThree(numbers []int) int {
 	total := 0
 
+    // Проходим по каждому числу в списке
 	for _, number := range numbers {
+        // Преобразуем число в строку и проходим по каждому символу
 		for _, digit := range strconv.Itoa(number) {
+            // Преобразуем символ обратно в число
 			d, _ := strconv.Atoi(string(digit))
+            // Если цифра делится на 3 без остатка, добавляем ее к общей сумме
 			if d%3 == 0 {
 				total += d
 			}
@@ -106,7 +126,10 @@ func sumOfDivisibleByThree(numbers []int) int {
 }
 
 func main() {
+    // Пример входных данных
 	numbers := []int{3, 333, 5670, 99}
+    // Выводим результат работы функции
 	fmt.Println(sumOfDivisibleByThree(numbers)) // Вывод: 36
 }
+
 */
