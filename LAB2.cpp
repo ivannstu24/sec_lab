420a52d557116c18aae23c3f7145783649f25cb8
ivannstu24 <161632314+ivannstu24@users.noreply.github.com>
2024-04-06 22:29:29 +0700

    Update LAB2.cpp

--- LAB2.cpp
+++ LAB2.cpp
@@ -1,4 +1,6 @@
+/*
 
+//задание 2
 #include <iostream>
 #include <unordered_set>
 #include <string>
@@ -39,6 +41,45 @@ int main() {
     // Создаем множество для хранения уникальных email-адресов
     std::unordered_set<std::string> uniqueEmails;
 
+*/
+
+/*
+
+//задание 3
+#include <iostream>
+#include <vector>
+#include <string>
+
+int sum_of_divisible_by_three(std::vector<int> numbers) {
+    // Инициализируем переменную для хранения суммы
+    int total = 0;
+
+    // Перебираем числа из вектора
+    for (int number : numbers) {
+        // Преобразуем число в строку и перебираем каждую его цифру
+        std::string num_str = std::to_string(number);
+        for (char digit : num_str) {
+            // Преобразуем символ цифры в целое число
+            int digit_int = digit - '0';
+            // Проверяем, делится ли текущая цифра на 3 без остатка
+            if (digit_int % 3 == 0) {
+                // Если делится, добавляем её к общей сумме
+                total += digit_int;
+            }
+        }
+    }
+    
+    // Возвращаем общую сумму
+    return total;
+}
+
+int main() {
+    // Пример использования:
+    std::vector<int> numbers = {3, 33333, 570, 99};
+    std::cout << sum_of_divisible_by_three(numbers) << std::endl;
+    return 0;
+}
+
     // Проверяем каждый email-адрес и добавляем его в множество, если он корректен
     for (const std::string& email : emails) {
         // Разбиваем email-адрес на имя пользователя и домен
@@ -69,3 +110,4 @@ int main() {
 
     return 0;
 }
+*/
