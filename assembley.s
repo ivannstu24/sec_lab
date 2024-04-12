fe95e748669b52ce60adb7ddfb103ff4b2f90bcf
ivannstu24 <161632314+ivannstu24@users.noreply.github.com>
2024-04-12 11:11:47 +0700

    Update assembley.s

--- assembley.s
+++ assembley.s
@@ -15,63 +15,46 @@ main:
     push rbp
     mov rbp, rsp
     
-    ; Read the number of elements
     mov rdi, fmt_read
     mov rsi, n
     xor rax, rax
     call scanf
 
 _n_loop:
-    ; Decrement the counter
-    dec dword [n]
+    dec byte [n]
 
-    ; Read the number
     mov rdi, fmt_read
     mov rsi, number
     xor rax, rax
     call scanf
 
-    ; Check if the number is divisible by 3
     mov eax, [number]
-    xor edx, edx
-    mov ecx, 3
-    div ecx
-    cmp edx, 0
-    jne _continue_loop ; If not divisible by 3, skip to the next number
+    mov r8, 3
+    xor rdx, rdx
+    div r8
+    cmp rdx, 0
+    je _is_divide_by_3
 
-    ; If divisible by 3, calculate the sum of its digits
-    mov eax, [number]
-    xor ecx, ecx ; Clear the sum register
-
-_sum_digits:
-    ; Extract the last digit
-    xor edx, edx
-    mov esi, 10
-    div esi
-
-    ; Add the digit to the sum
-    add ecx, edx
-
-    ; Check if there are more digits
-    cmp eax, 0
-    jne _sum_digits
-
-    ; Add the sum of digits to the count
-    add [count], ecx
-
-_continue_loop:
-    ; Check if we have processed all numbers
     mov eax, [n]
     cmp eax, 0
-    jne _n_loop
+    je _print_func
+    jmp _n_loop
 
-    ; Print the count
+_is_divide_by_3:
+    mov eax, [number]
+    add r8d, eax
+    mov [count], r8d
+    mov eax, [n]
+    cmp eax, 0
+    je _print_func
+    jmp _n_loop
+
+_print_func:
     mov rdi, fmt_write
     mov esi, [count]
     xor rax, rax
     call printf
 
-    ; Exit the program
     mov eax, 60
-    xor edi, edi
+    xor rdi, rdi
     syscall
