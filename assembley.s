d78363b5d791202e702c5df839b24032bb30cf2b
ivannstu24 <161632314+ivannstu24@users.noreply.github.com>
2024-04-12 10:58:22 +0700

    Update assembley.s

--- assembley.s
+++ assembley.s
@@ -1,56 +1,77 @@
 global main
 
 section .data
-    n: dq 0                   ; Указатель на число
-    number: dq 0              ; Текущее число для разбора
-    count: dq 0               ; Сумма цифр, делящихся на 3
-    fmt_read: db "%lld", 0    ; Формат строки для чтения числа
-    fmt_write: db "%lld\n", 0 ; Формат строки для вывода суммы
+    n: dd 0
+    number: dd 0
+    count: dd 0
+    fmt_read: db "%d", 0
+    fmt_write: db "%d", 10, 0
+
+default rel
+extern printf, scanf
 
 section .text
 main:
     push rbp
     mov rbp, rsp
-
+    
     mov rdi, fmt_read
-    mov rsi, qword [n]
+    mov rsi, n
     xor rax, rax
     call scanf
 
 _n_loop:
+    dec dword [n]
+
     mov rdi, fmt_read
-    mov rsi, qword [number]
+    mov rsi, number
     xor rax, rax
     call scanf
 
-    mov rax, qword [number]
-    mov rcx, 3
+    mov eax, [number]
+    mov r8, 3
     xor rdx, rdx
-    div rcx
+    div r8
     cmp rdx, 0
     je _is_divide_by_3
 
-    mov rax, qword [n]
-    cmp rax, 0
+    mov eax, [n]
+    cmp eax, 0
     je _print_func
     jmp _n_loop
 
 _is_divide_by_3:
-    mov rax, qword [number]
-    mov rcx, qword [count]
-    add rcx, rax
-    mov [count], rcx
-    mov rax, qword [n]
-    cmp rax, 0
+    ; Reset the sum of digits for each number
+    xor ecx, ecx
+    mov eax, [number]
+
+_sum_digits:
+    ; Extract the last digit
+    xor edx, edx
+    mov r8, 10
+    div r8
+
+    ; Add the digit to the sum
+    add ecx, edx
+
+    ; Check if there are more digits
+    cmp eax, 0
+    jne _sum_digits
+
+    ; Add the sum of digits to the count
+    add [count], ecx
+
+    mov eax, [n]
+    cmp eax, 0
     je _print_func
     jmp _n_loop
 
 _print_func:
     mov rdi, fmt_write
-    mov rsi, qword [count]
+    mov esi, [count]
     xor rax, rax
     call printf
 
-    mov rax, 60
+    mov eax, 60
     xor rdi, rdi
     syscall
