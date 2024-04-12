245dc39613d7dd8568be187f42b6d1ec17718f91
ivannstu24 <161632314+ivannstu24@users.noreply.github.com>
2024-04-12 10:35:56 +0700

    Update assembley.s

--- assembley.s
+++ assembley.s
@@ -3,7 +3,7 @@ global main
 section .data
     n: dd 0
     number: dd 0
-    sum: dd 0
+    count: dd 0
     fmt_read: db "%d", 0
     fmt_write: db "%d", 10, 0
 
@@ -15,54 +15,71 @@ main:
     push rbp
     mov rbp, rsp
     
-    ; Считываем количество чисел
     mov rdi, fmt_read
     mov rsi, n
     xor rax, rax
     call scanf
 
 _n_loop:
-    ; Уменьшаем счетчик чисел
     dec dword [n]
 
-    ; Считываем число
     mov rdi, fmt_read
     mov rsi, number
     xor rax, rax
     call scanf
 
-    ; Инициализируем сумму цифр для текущего числа
-    xor ecx, ecx
     mov eax, [number]
+    xor ecx, ecx ; Счетчик цифр
+    xor edx, edx ; Текущая цифра
 
-_digit_sum:
-    ; Извлекаем последнюю цифру числа
-    xor edx, edx
+_digit_loop:
     mov ebx, 10
-    div ebx
+    div ebx ; Деление на 10 для извлечения цифры
+    mov edx, 0
+    cmp edx, 0
+    je _next_digit
+    mov eax, ecx
+    mul ebx
+    add eax, edx
+    mov ecx, eax
 
-    ; Добавляем цифру к сумме
-    add ecx, edx
-
-    ; Проверяем, остались ли еще цифры
+_next_digit:
     cmp eax, 0
-    jne _digit_sum
+    jne _digit_loop
 
-    ; Добавляем сумму цифр к общей сумме
-    add [sum], ecx
+    ; Проверка каждой из цифр на делимость на 3
+    mov eax, ecx
+    xor ecx, ecx
 
-    ; Проверяем, остались ли еще числа для обработки
-    cmp dword [n], 0
-    jne _n_loop
+_check_divisibility:
+    mov ebx, 10
+    div ebx ; Деление на 10 для получения следующей цифры
+    mov edx, 0
+    cmp edx, 0
+    je _next_check
+    mov eax, ecx
+    mul ebx
+    add eax, edx
+    mov ecx, eax
+
+_next_check:
+    cmp eax, 0
+    jne _check_divisibility
+
+    ; Добавление к общей сумме
+    add [count], ecx
+
+    mov eax, [n]
+    cmp eax, 0
+    je _print_func
+    jmp _n_loop
 
 _print_func:
-    ; Выводим общую сумму цифр
     mov rdi, fmt_write
-    mov esi, [sum]
+    mov esi, [count]
     xor rax, rax
     call printf
 
-    ; Завершаем программу
     mov eax, 60
     xor rdi, rdi
     syscall
